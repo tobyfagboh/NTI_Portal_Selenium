70713f5fe97f8a4f56fb53c7c892ab8be647ad2c
kennyinspire <adunola.adeyemi@gmail.com>
2019-03-17 22:33:28 +0100

    NW-A2: Verification of HomePage

--- src/test/java/com/w2a/testcase/homepage/homepage.java
+++ src/test/java/com/w2a/testcase/homepage/homepage.java
@@ -1,5 +1,20 @@
 package com.w2a.testcase.homepage;
 
-public class homepage {
+import java.util.Properties;
 
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+	
+	public	class homepage extends TestBase {
+		Properties properties;
+		@Test
+		public void verifyhomepage() throws InterruptedException {
+			
+			//log.debug("Log in page Executed");
+			
+						
+		}
 }
+
