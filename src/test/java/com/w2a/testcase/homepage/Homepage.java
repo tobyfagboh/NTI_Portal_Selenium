d15b9ff18a2eb68be40232d98965e5f851831dcd
kennyinspire <adunola.adeyemi@gmail.com>
2019-04-08 16:18:24 +0100

    NW-F30: Home page run in parallel

--- src/test/java/com/w2a/testcase/homepage/Homepage.java
+++ src/test/java/com/w2a/testcase/homepage/Homepage.java
@@ -1,13 +1,16 @@
 package com.w2a.testcase.homepage;
 
+import java.util.Properties;
+
 import org.testng.annotations.Test;
 
 import com.w2a.base.TestBase;
 
-public class Homepage extends TestBase{
+	public class Homepage extends TestBase{
+		Properties properties; 
 
 	@Test (priority=1)
-	public void howToApply () throws InterruptedException  {
+	public void verifyHowToApply() throws InterruptedException  {
 
 		log.debug("Log in page Executed");
 		Thread.sleep(3000);
@@ -18,8 +21,8 @@ public class Homepage extends TestBase{
 		driver.quit();
 	}
 	
-	@Test(priority=2)
-	public void howToRegister () throws InterruptedException  {
+	@Test (priority=2)
+	public void verifyHowToRegister() throws InterruptedException  {
 
 		log.debug("Log in page Executed");
 		Thread.sleep(3000);
@@ -30,8 +33,33 @@ public class Homepage extends TestBase{
 		driver.quit();
 
 }
-	@Test(priority=3)
-	public void verifyPrivacyPolicy () throws InterruptedException  {
+	@Test (priority=3)
+	public void verifyHelpAndSupportPage() throws InterruptedException  {
+
+		log.debug("Log in page Executed");
+		Thread.sleep(3000);
+		click("helpAndSupportPageBtn_XPATH");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.quit();
+
+}
+	
+	@Test (priority=4)
+	public void verifyTermsOfUser() throws InterruptedException  {
+
+		log.debug("Log in page Executed");
+		Thread.sleep(3000);
+		click("termsOfUserBtn_XPATH");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.quit();
+}
+	
+	@Test (priority=5)
+	public void verifyPrivacyPolicy() throws InterruptedException  {
 
 		log.debug("Log in page Executed");
 		Thread.sleep(3000);
@@ -42,60 +70,6 @@ public class Homepage extends TestBase{
 		driver.quit();
 }
 	
-	@Test(priority=4)
-	public void verifyTermsOfUser () throws InterruptedException  {
-
-		log.debug("Log in page Executed");
-		Thread.sleep(3000);
-		click("termsOfUserBtn_XPATH");
-		Thread.sleep(3000);
-		driver.navigate().back();
-		Thread.sleep(3000);
-		driver.quit();
-}
-	
-	@Test(priority=5)
-	public void verifyHomeAndSupport () throws InterruptedException {
-		
-		log.debug("Log in page Executed");
-		Thread.sleep(3000);
-		click("homeAndSupportBtn_XPATH");
-		Thread.sleep(3000);
-		driver.navigate().back();
-		
-	}
-	
-	@Test(priority = 6)
-	public void verifyHowToApply  () throws InterruptedException {
-		
-		log.debug("Log in page Executed");
-		Thread.sleep(3000);
-		click("howToApplyBtn_XPATH");
-		Thread.sleep(3000);
-		driver.navigate().back();
-	}
-
-	@Test
-	public void verifyHowToRegister () throws InterruptedException {
-		
-		log.debug("Log in page Executed");
-		Thread.sleep(3000);
-		click("howToRegisterBtn_XPATH");
-		Thread.sleep(3000);
-		driver.navigate().back();
-		
-	}
-	
-	@Test
-	public void verifyTermOfUser () throws InterruptedException {
-		
-		log.debug("Log in page Executed");
-		Thread.sleep(3000);
-		click("termsOfUserBtn_XPATH");
-		Thread.sleep(3000);
-		driver.navigate().back();
-		driver.quit();
-	}
 }
 
 
