fb33ada637ef9fb2119cc2caa25648e17a2f0823
kennyinspire <adunola.adeyemi@gmail.com>
2019-04-15 02:02:15 +0100

    NW-F: Update on Homepage to run in parallel

--- src/test/java/com/w2a/testcase/homepage/Homepage.java
+++ src/test/java/com/w2a/testcase/homepage/Homepage.java
@@ -17,8 +17,6 @@ import com.w2a.base.TestBase;
 		click("howToApplyPageBtn_XPATH");
 		Thread.sleep(3000);
 		driver.navigate().back();
-		Thread.sleep(3000);
-		driver.quit();
 	}
 	
 	@Test (priority=2)
@@ -26,24 +24,19 @@ import com.w2a.base.TestBase;
 
 		log.debug("Log in page Executed");
 		Thread.sleep(3000);
-		click("howToRegisterPageBtn_PATH");
+		click("howToRegisterPageBtn_XPATH");
 		Thread.sleep(3000);
 		driver.navigate().back();
-		Thread.sleep(3000);
-		driver.quit();
-
-}
+	}
+	
 	@Test (priority=3)
-	public void verifyHelpAndSupportPage() throws InterruptedException  {
+	public void verifyHelpAndSupportPage () throws InterruptedException  {
 
 		log.debug("Log in page Executed");
 		Thread.sleep(3000);
 		click("helpAndSupportPageBtn_XPATH");
 		Thread.sleep(3000);
 		driver.navigate().back();
-		Thread.sleep(3000);
-		driver.quit();
-
 }
 	
 	@Test (priority=4)
@@ -54,8 +47,6 @@ import com.w2a.base.TestBase;
 		click("termsOfUserBtn_XPATH");
 		Thread.sleep(3000);
 		driver.navigate().back();
-		Thread.sleep(3000);
-		driver.quit();
 }
 	
 	@Test (priority=5)
